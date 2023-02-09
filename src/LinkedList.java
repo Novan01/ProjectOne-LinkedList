75824d379bd3fff6491c0ff28f62a34dcfec5c8c
Novan01 <patrick@casaanderson.net>
2023-02-09 16:59:51 -0600

    commit:   bin/LinkedList$Node.class 	commit:   bin/LinkedList.class 	commit:    bin/Node.class 	commit:   bin/Product.class 	commit:   src/LinkedList.java 	commit:    src/Node.java 	commit:   src/Product.java

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -4,6 +4,21 @@ import java.io.*;
 
 //custom singly linked list class that impplements IDedObject interface
 public abstract class LinkedList<T> implements IDedObject<T> {
+	Node<T> head;
+
+	static class Node<T> {
+		int data;
+		Node<T> next;
+
+		Node(int d) {
+			data = d;
+			next = null;
+		}
+	}
+
+	public LinkedList() {
+		head = null;
+	}
 
 	// clear out the whole linked list
 	public void makeEmpty() {
@@ -18,7 +33,22 @@ public abstract class LinkedList<T> implements IDedObject<T> {
 
 	// insert node at the fron
 	public boolean insertAtFront(Node<T> x) {
-		return false;
+		Node<T> new_head = x; // new head node is the node we want to inset
+		new_head.next = null; // the pointer to the next is now pointing at old head
+
+		// if there is no head node
+		if (head == null) {
+			head = new_head;
+		}
+		// else, traverse the list to find the next place to put the old head node
+		else {
+			Node last = head;
+			while (last.next != null) {
+				last = last.next;
+			}
+			last.next = new_head;
+		}
+		return true;
 	}
 
 	// delete front node
@@ -28,12 +58,28 @@ public abstract class LinkedList<T> implements IDedObject<T> {
 
 	// delete node with ID - int ID
 	public T delete(int ID) {
+		Node<T> deleteNode = new Node<T>(ID); // create node for what we want to delete
+		deleteNode.next = null;
+
+		Node<T> currNode = head;
+
+		while (currNode != null) {
+			if (currNode.data == deleteNode.data) {
+				currNode = null;
+			}
+		}
 		return null;
 	}
 
 	// traverse the linked list and print all nodes and data
 	public void printAllRecords() {
-
+		Node<T> currNode = head;
+		Node<T> nextNode = head.next;
+		while (currNode != null) {
+			System.out.println(currNode);
+			currNode = nextNode;
+			nextNode = nextNode.next;
+		}
 	}
 
 }
--- src/Product.java
+++ src/Product.java
@@ -3,25 +3,25 @@
 //implements IDedObject interface
 import java.util.*;
 
-public class Product<T> implements IDedObject<T>{
+public class Product<T> implements IDedObject<T> {
 
-	//required variables
-	private int productID; //ID of the product object
-	private String productName; //name of the product object
-	private String supplierName; //name of the supplier of the product object
-	
+	// required variables
+	private int productID; // ID of the product object
+	private String productName; // name of the product object
+	private String supplierName; // name of the supplier of the product object
+
 	public static void main(String[] args) {
-		
+		LinkedList<T> list;
 
 	}
 
-	
-	//implementation of interface method getID - returns the Id of the object
+	// implementation of interface method getID - returns the Id of the object
 	public int getID() {
 		return productID;
 	}
 
-	//implementation of interface method printID - print details of ID seperate lines
+	// implementation of interface method printID - print details of ID seperate
+	// lines
 	public void printID() {
 		System.out.println(productID + "\n" + productName + "\n" + supplierName);
 	}
