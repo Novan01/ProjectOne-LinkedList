a632c83177b6661b56529a1f41ae04b68a22c933
Novan01 <patrick@casaanderson.net>
2023-02-09 16:06:38 -0600

    Patrick-branch

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,40 +1,39 @@
+
 //Linked List class
 import java.io.*;
 
 //custom singly linked list class that impplements IDedObject interface
-public abstract class LinkedList<T> implements IDedObject<T>{
-	
-	//clear out the whole linked list
+public abstract class LinkedList<T> implements IDedObject<T> {
+
+	// clear out the whole linked list
 	public void makeEmpty() {
-		
+		// this method will make things empty
 	}
-	
-	
-	//traverse the linked list to find node with ID ID
+
+	// traverse the linked list to find node with ID ID
 	public T findID(int ID) {
 		return null;
-		
+
 	}
-	
-	//insert node at the fron
+
+	// insert node at the fron
 	public boolean insertAtFront(Node<T> x) {
 		return false;
 	}
-	
-	//delete front node
+
+	// delete front node
 	public T deleteFromFront() {
 		return null;
 	}
-	
-	//delete node with ID - int ID
+
+	// delete node with ID - int ID
 	public T delete(int ID) {
 		return null;
 	}
-	
-	//traverse the linked list and print all nodes and data
-	public void printAllRecords( ) {
-		
+
+	// traverse the linked list and print all nodes and data
+	public void printAllRecords() {
+
 	}
-	
-	
+
 }
